5e42962ce93737cecfbf6961aa602911c765e5fa
Carlos Eduardo <166997808+Offduduzzz@users.noreply.github.com>
2024-04-21 05:40:17 -0300

    validação de cpf

--- validação_de_cpf_em_python/main.py
+++ validação_de_cpf_em_python/main.py
@@ -3,37 +3,42 @@
         1 1 1 4 4 4 7 7 7 3 5
       -11 10 9 8 7 6 5 4 3 2 1
       '''
+#    cpf: 54565323010
+#    Primeiro digito
+cpf_usuario = input('Digite o CPF: ')
+nove_digitos_1 = cpf_usuario[:9]
 
+contador_regresivo_1 = 10
+resultado_digito_1 = 0
 
-print('Validação de CPF')
+for digito_1 in nove_digitos_1:
+    resultado_digito_1 += int(digito_1) * contador_regresivo_1
+    contador_regresivo_1 -= 1
+digito_1 = resultado_digito_1 % 11
+if digito_1 < 2:
+    digito_1 = 0
+else:
+    digito_1 = (11 - digito_1)
 
-cpf = []
-cpf_copia = cpf.copy()
+#    Segundo digito
 
-#   loop infinito
-while True:
-    cpf = input('Digite seus primeiros 9 digitos do CPF (EX.: 111444777) : ')
-    cpf_int = 0
-    #   checagem do número degitado
-    try:
-        cpf_int = int(cpf)
-        list.cpf = cpf_int
-        #   amostrar do dígitos do cpf e seus respectivos indices
-        for i in cpf_int:
-            print(f'{i}')
-        #   ato da validação
+dez_digitos_2 = cpf_usuario[:10]
 
-        print(cpf[0]*2)
-        print(cpf[1]*3)
-        print(cpf[2]*4)
-        print(cpf[3]*5)
-        print(cpf[4]*6)
-        print(cpf[5]*7)
-        print(cpf[6]*8)
-        print(cpf[7]*9)
-        print(cpf[8]*10)
+contador_regresivo_2 = 11
+resultado_digito_2 = 0
 
+for digito_2 in dez_digitos_2:
+    resultado_digito_2 += int(digito_2) * contador_regresivo_2
+    contador_regresivo_2 -= 1
+digito_2 = resultado_digito_2 % 11
+if digito_2 < 2:
+    digito_2 = 0
+else:
+    digito_2 = (11 - digito_2)
 
-    except ValueError:
-        print('Você não digitou um CPF. Digite novamente!')
+cpf_gerado_pelo_calculo = f'{nove_digitos_1}{digito_1}{digito_2}'
 
+if cpf_usuario == cpf_gerado_pelo_calculo:
+    print(f'{cpf_usuario} é válido')
+else:
+    print('CPF inválido')
